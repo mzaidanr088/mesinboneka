768267b1c62ffb3e1420a2af037c3b513feb65dc
Zaidan <Muhammadzaidan088@gmail.com>
2021-01-21 06:35:09 +0700

    update

--- mesinboneka.php
+++ mesinboneka.php
@@ -1,6 +1,8 @@
 <?php
-
-$mesinboneka = [[
+include("doll.php");
+	$dollmachine = new doll();
+	
+$dollmachine = [[
 	      "jenisboneka" => "Bear","Eagle","Tiger","Dragon","Lion"
 	],
            "jenisvariasi" => "hat","stick","sword","shild","magic ring","angel wings",
