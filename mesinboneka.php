fe0a835dbd2b9214e534af8cc24fca62d9e5e13a
Zaidan <Muhammadzaidan088@gmail.com>
2021-01-21 06:27:26 +0700

    update

--- mesinboneka.php
+++ mesinboneka.php
@@ -1,2 +1,8 @@
 <?php
 
+$mesinboneka = [[
+	      "jenisboneka" => "Bear","Eagle","Tiger","Dragon","Lion"
+	],
+           "jenisvariasi" => "hat","stick","sword","shild","magic ring","angel wings",
+];
+
